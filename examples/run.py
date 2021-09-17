d29c2b5c70322b9a484ac3898829771ef6f7da7f
Andy Lok <andylokandy@hotmail.com>
2021-09-17 17:09:58 +0800

    build with manylinux2010
    Signed-off-by: Andy Lok <andylokandy@hotmail.com>

--- examples/run.py
+++ examples/run.py
@@ -1,18 +1,15 @@
-from tikv_client import TransactionClient
+from tikv_client import RawClient
 
-client = TransactionClient.connect("127.0.0.1:2379")
+client = RawClient.connect("127.0.0.1:2379")
 
-txn = client.begin(pessimistic=True)
-txn.put(b"k1", b"v1")
-txn.put(b"k2", b"v2")
-txn.put(b"k3", b"v3")
-txn.put(b"k4", b"v4")
-txn.put(b"k5", b"v5")
-txn.commit()
+client.put(b"k1", b"v1")
+client.put(b"k2", b"v2")
+client.put(b"k3", b"v3")
+client.put(b"k4", b"v4")
+client.put(b"k5", b"v5")
 
-snapshot = client.snapshot(client.current_timestamp(), pessimistic=True)
-print(snapshot.get(b"k3"))
-print(snapshot.batch_get([b"k1", b"k4"]))
+print(client.get(b"k3"))
+print(client.batch_get([b"k1", b"k4"]))
 
-for k, v in snapshot.scan(b"k1", end=None, limit=10, include_start=False):
+for k, v in client.scan(b"k1", end=None, limit=10, include_start=False):
     print(k, v)
